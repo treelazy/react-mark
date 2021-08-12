5bbb66b522bb0a5b5d5e9f9e062c90a593d7ca72
Mark_Liao <lyk024@hotmail.com>
2021-08-12 11:42:08 +0800

    fix: 修正Regex的判斷錯誤

--- src/components/TrainingTwo/validationSchema.js
+++ src/components/TrainingTwo/validationSchema.js
@@ -13,7 +13,7 @@ export const valuesSchema = Yup.object().shape({
         })
     }),
     organizationName: Yup.string().max(15, "請輸入1-15個中文，英文/數字").matches(/^[\u4e00-\u9fa5<>/\w]*$/, "請輸入中文，英文/數字"),
-    weight: Yup.string().matches(/^(0|[1-9][0-9]*)(.[0-9]{1,2})?$/, "請輸入半形數字 0~9999999.99/小數點後限2位").test("", "請輸入半形數字 0~9999999.99/小數點後限2位", (v) => !v || Number(v) <= 999999999.99),
+    weight: Yup.string().matches(/^(0|[1-9][0-9]*)(\.[0-9]{1,2})?$/, "請輸入半形數字 0~9999999.99/小數點後限2位").test("", "請輸入半形數字 0~9999999.99/小數點後限2位", (v) => !v || Number(v) <= 999999999.99),
     price: Yup.number().required("此欄位必填"),
     description: Yup.string().required("此欄位必填").test("", "請輸入1-3000個字", (str) => !str || getDescriptionLength(str) <= 3000).test("", "此欄位不支援斷行", (str) => !str || str.search(/\n/) === -1),
     instruction: Yup.string().max(15, "請輸入1-15個中文、半形英文/數字/特殊符號").matches(/^[^ 　]+$/, "此欄位不支援空白").matches(/^[a-zA-Z0-9\u4e00-\u9fff\u0021-\u002F\u003A-\u0040\u005B-\u0060\u007B-\u007E\u3000\u3001-\u303F]+$/, "請輸入中文、半形英文/數字/特殊符號"),
